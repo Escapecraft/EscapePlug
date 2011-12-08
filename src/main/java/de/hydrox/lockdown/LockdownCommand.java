97b9a493221831b07a77c16264dabd8698dfea23
Matthias Söhnholz <miniminko@googlemail.com>
2011-12-08 20:57:18 +0100

    initial commit on Lockdown-mode
    - lockdown mode can be enabled and disabled
    - only players with special permission can join while server is in
      lockdown
    - only players with special permission can change lockdown
    - players recieve a message on login if the server is in lockdown

---
+++ src/main/java/de/hydrox/lockdown/LockdownCommand.java
@@ -0,0 +1,32 @@
+package de.hydrox.lockdown;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class LockdownCommand implements CommandExecutor {
+
+	LockdownListener listener = null;
+	
+	public LockdownCommand(LockdownListener listener) {
+		this.listener = listener;
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (!(sender.hasPermission("escapeplug.lockdown.change"))) {
+			sender.sendMessage(ChatColor.RED + "You don't have permission to change lockdown.");
+			return true;
+		}
+		if (args[0].equalsIgnoreCase("enable")) {
+			this.listener.isLockdownActive = true;
+			return true;
+		}
+		if (args[0].equalsIgnoreCase("disable")) {
+			this.listener.isLockdownActive = false;
+			return true;
+		}
+		return false;
+	}
+
+}
