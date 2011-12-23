08b037d8186c727bf1f4c99aa1ca7bfde4e5b3a8
Matthias Söhnholz <miniminko@googlemail.com>
2011-12-23 23:06:13 +0100

    add player notification for lockdown changes
    only players with the escapecraft.lockdown.notify permission get
    notified

--- src/main/java/de/hydrox/lockdown/LockdownCommand.java
+++ src/main/java/de/hydrox/lockdown/LockdownCommand.java
@@ -1,9 +1,11 @@
 package de.hydrox.lockdown;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class LockdownCommand implements CommandExecutor {
 
@@ -20,10 +22,22 @@ public class LockdownCommand implements CommandExecutor {
 		if (args.length >= 1) {
 			if (args[0].equalsIgnoreCase("enable")) {
 				this.listener.isLockdownActive = true;
+				Player[] players = Bukkit.getOnlinePlayers();
+				for (Player player : players) {
+					if(player.hasPermission("escapeplug.lockdown.notify")) {
+						player.sendMessage(ChatColor.RED + "LOCKDOWN-MODE ACTIVATED.");
+					}
+				}
 				return true;
 			}
 			if (args[0].equalsIgnoreCase("disable")) {
 				this.listener.isLockdownActive = false;
+				Player[] players = Bukkit.getOnlinePlayers();
+				for (Player player : players) {
+					if(player.hasPermission("escapeplug.lockdown.notify")) {
+						player.sendMessage(ChatColor.GREEN + "Lockdown-Mode deactivated.");
+					}
+				}
 				return true;
 			}
 		}
