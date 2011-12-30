bd79c20578df5deda29c7654612afa4865c40c0f
Matthias Söhnholz <miniminko@googlemail.com>
2011-12-31 02:12:06 +0800

    add /who and /who <name>

--- src/main/java/net/escapecraft/escapePlug/EscapePlug.java
+++ src/main/java/net/escapecraft/escapePlug/EscapePlug.java
@@ -10,9 +10,12 @@ import org.bukkit.event.entity.EntityListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.hydrox.antiSlime.SlimeDamageListener;
+import de.hydrox.bukkit.DroxPerms.DroxPerms;
+import de.hydrox.bukkit.DroxPerms.DroxPermsAPI;
 import de.hydrox.bukkit.timezone.TimezoneCommands;
 import de.hydrox.lockdown.LockdownCommand;
 import de.hydrox.lockdown.LockdownListener;
+import de.hydrox.who.WhoCommand;
 import en.tehbeard.endernerf.EndernerfListener;
 import en.tehbeard.gamemode.GameModeToggle;
 import en.tehbeard.mentorTeleport.MentorBack;
@@ -25,9 +28,11 @@ import en.tehbeard.reserve.ReserveListener;
 public class EscapePlug extends JavaPlugin {
 
 	private static final Logger log = Logger.getLogger("Minecraft");
+
+	private DroxPermsAPI droxPermsAPI = null;
+
 	public static EscapePlug self = null;
-	//Hatme config variables
-	
+
 	public void onEnable() {
 		self = this;
 		log.info("[EscapePlug] loading EscapePlug");
@@ -36,14 +41,19 @@ public class EscapePlug extends JavaPlugin {
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 
+		DroxPerms droxPerms = ((DroxPerms) this.getServer().getPluginManager().getPlugin("DroxPerms"));
+		if (droxPerms != null) {
+			droxPermsAPI = droxPerms.getAPI();
+		}
+
 		//Starting reserve list
-		if(getConfig().getBoolean("plugin.reserve.enabled", true)){
+		if (getConfig().getBoolean("plugin.reserve.enabled", true)) {
 			ReserveListener rl = new ReserveListener();
 			this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_LOGIN, rl, Event.Priority.Highest, this);
 		}
 
 		//start loading AntiSlime
-		if(getConfig().getBoolean("plugin.antislime.enabled", true)){
+		if (getConfig().getBoolean("plugin.antislime.enabled", true)) {
 			log.info("[EscapePlug] loading AntiSlime");
 			SlimeDamageListener slimeDamageListener = new SlimeDamageListener();
 			this.getServer().getPluginManager().registerEvent(Event.Type.ENTITY_TARGET, slimeDamageListener, Event.Priority.Normal, this);
@@ -54,7 +64,7 @@ public class EscapePlug extends JavaPlugin {
 		//finished loading AntiSlime
 
 		//start loading MentorTeleport
-		if(getConfig().getBoolean("plugin.mentortp.enabled", true)){
+		if (getConfig().getBoolean("plugin.mentortp.enabled", true)) {
 			log.info("[EscapePlug] loading MentorTP");
 			getCommand("mentortp").setExecutor(new MentorTeleport(this));
 			getCommand("mentorback").setExecutor(new MentorBack(this));
@@ -64,15 +74,12 @@ public class EscapePlug extends JavaPlugin {
 		}
 
 		//start loading PigJouster
-		if(getConfig().getBoolean("plugin.pigjoust.enabled", true)){
+		if (getConfig().getBoolean("plugin.pigjoust.enabled", true)) {
 			log.info("[EscapePlug] loading PigJouster");
 			getCommand("pig-active").setExecutor(new PigJouster());
 			getCommand("pig-deactive").setExecutor(new PigJouster());
 			PigListener pigListener = new PigListener();
 			PigPlayerListener pigPlayerListener = new PigPlayerListener();
-
-
-
 
 			this.getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DAMAGE, pigListener, Event.Priority.Normal, this);
 			this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_INTERACT_ENTITY, pigPlayerListener, Event.Priority.Normal, this);
@@ -82,7 +89,7 @@ public class EscapePlug extends JavaPlugin {
 		}
 
 		//start loading Timezone
-		if(getConfig().getBoolean("plugin.timezone.enabled", true)){
+		if (getConfig().getBoolean("plugin.timezone.enabled", true)) {
 			log.info("[EscapePlug] loading Timezone");
 			getCommand("timezone").setExecutor(new TimezoneCommands());
 			//finished loading Timezone
@@ -90,26 +97,25 @@ public class EscapePlug extends JavaPlugin {
 			log.info("[EscapePlug] skipping Timezone");
 		}
 
-
 		//start loading togglemode
-		if(getConfig().getBoolean("plugin.togglemode.enabled", true)){
+		if (getConfig().getBoolean("plugin.togglemode.enabled", true)) {
 			log.info("[EscapePlug] loading ToggleGameMode");
 			getCommand("togglemode").setExecutor(new GameModeToggle());
 			//finished loading togglemode
 		}
 
 		//start loading endernerf
-		if(getConfig().getBoolean("plugin.endernerf.enabled", true)){
+		if (getConfig().getBoolean("plugin.endernerf.enabled", true)) {
 			log.info("[EscapePlug] loading enderNerf");
 			EntityListener el = new EndernerfListener();
 			this.getServer().getPluginManager().registerEvent(Event.Type.ENDERMAN_PICKUP, el, Event.Priority.Highest, this);
 			this.getServer().getPluginManager().registerEvent(Event.Type.ENDERMAN_PLACE, el, Event.Priority.Highest, this);
-		
+
 			//finished loading endernerf
 		}
 
 		//start loading lockdown
-		if(getConfig().getBoolean("plugin.lockdown.enabled", true)){
+		if (getConfig().getBoolean("plugin.lockdown.enabled", true)) {
 			log.info("[EscapePlug] loading Emergency Lockdown");
 			LockdownListener lockdownListener = new LockdownListener(this);
 			getCommand("lockdown").setExecutor(new LockdownCommand(lockdownListener));
@@ -117,11 +123,11 @@ public class EscapePlug extends JavaPlugin {
 			this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, lockdownListener, Event.Priority.Highest, this);
 			//finished loading lockdown
 		}
-		
+
 		//start loading hatMe
-		if(getConfig().getBoolean("plugin.hatme.enabled", true)){
+		if (getConfig().getBoolean("plugin.hatme.enabled", true)) {
 			log.info("[EscapePlug] loading hatMe");
-			
+
 			//get Config
 			List<Integer> rbBlocks = getConfig().getIntegerList("plugin.hatme.allowed");
 			boolean rbAllow = getConfig().getBoolean("plugin.hatme.enable");
@@ -130,23 +136,28 @@ public class EscapePlug extends JavaPlugin {
 			String hatversion = getConfig().getString("plugin.hatme.hatversion");
 
 			//construct command and assign to /hat and /unhat
-			HatmeCommand hatMe = new HatmeCommand(rbBlocks,rbAllow,notAllowedMsg,rbOp);
+			HatmeCommand hatMe = new HatmeCommand(rbBlocks, rbAllow, notAllowedMsg, rbOp);
 			getCommand("hat").setExecutor(hatMe);
 			getCommand("unhat").setExecutor(hatMe);
 			log.info("[EscapePlug] loaded hatMe version " + hatversion);
 		}
 
-
+		//start loading who
+		if (getConfig().getBoolean("plugin.who.enabled", true)) {
+			log.info("[EscapePlug] loading Who");
+			getCommand("who").setExecutor(new WhoCommand(droxPermsAPI));
+			//finished loading who
+		}
 
 		log.info("[EscapePlug] EscapePlug loaded");
 	}
 
 	public void onDisable() {
-		self=null;
+		self = null;
 		log.info("[EscapePlug] EscapePlug unloaded");
 	}
 
-	public static void printCon(String line){
-		log.info("[EscapePlug] "+line);
+	public static void printCon(String line) {
+		log.info("[EscapePlug] " + line);
 	}
 }
