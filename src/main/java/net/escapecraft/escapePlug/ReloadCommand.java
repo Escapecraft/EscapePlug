45cffad32f44935626dbbdb759b406f97eecad38
James <james@BeardNet.(none)>
2012-01-10 17:35:00 +0000

    Added reload command

---
+++ src/main/java/net/escapecraft/escapePlug/ReloadCommand.java
@@ -0,0 +1,17 @@
+package net.escapecraft.escapePlug;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ReloadCommand implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String lbl,
+			String[] args) {
+		// TODO Auto-generated method stub
+		
+		return true;
+	}
+
+}
