2d0ab97a9d43e37f68adf06cebe55ce71ab77768
Tulonsae <admin@tulonsae.org>
2012-08-05 00:17:40 -0700

    Completed the initial version of the antixray plugin. This handles the block updates.

--- src/main/java/org/tulonsae/antixray/AntiXrayComponent.java
+++ src/main/java/org/tulonsae/antixray/AntiXrayComponent.java
@@ -1,7 +1,11 @@
 package org.tulonsae.antixray;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.World;
 import org.tulonsae.mc.util.Log;
 
 import net.escapecraft.component.AbstractComponent;
@@ -19,12 +23,78 @@ public class AntiXrayComponent extends AbstractComponent {
         this.log = log;
         this.plugin = plugin;
 
-        //Bukkit.getPluginManager().registerEvents(this, plugin);
+        Bukkit.getPluginManager().registerEvents(new BlockBreakListener(this), plugin);
 
 	return true;
     }
 
     @Override
     public void disable() {
+    }
+
+    protected Log getLogger() {
+        return log;
+    }
+
+    protected EscapePlug getPlugin() {
+        return plugin;
+    }
+
+    /**
+     * Determine if the specified world is being monitored for antixray.
+     *
+     * @param world the world to check
+     * @return true if the world is monitored, otherwise false
+     */
+    protected static boolean isMonitoredWorld(World world) {
+        // TODO - implement this
+
+        return true;
+    }
+
+    /**
+     * Update the player with new blocks around the location specified.
+     *
+     * @param player the online player to update
+     * @param block the block which has been removed
+     */
+    protected static void updatePlayer(Player player, Block block) {
+        // update block below
+        if (block.getLocation().getBlockY() != 0) {
+            updateAdjacentBlock(player, block, BlockFace.DOWN);
+        }
+
+        // update block over
+        if (block.getLocation().getBlockY() != 255) {
+            updateAdjacentBlock(player, block, BlockFace.UP);
+        }
+
+        // update block sides
+        updateAdjacentBlock(player, block, BlockFace.NORTH);
+        updateAdjacentBlock(player, block, BlockFace.SOUTH);
+        updateAdjacentBlock(player, block, BlockFace.WEST);
+        updateAdjacentBlock(player, block, BlockFace.EAST);
+    }
+
+    // update adjacent block
+    private static void updateAdjacentBlock(Player player, Block block, BlockFace face) {
+        Block adjBlock = block.getRelative(face);
+        int typeId = adjBlock.getTypeId();
+        if (isProtectedBlock(typeId)) {
+            player.sendBlockChange(adjBlock.getLocation(), typeId, adjBlock.getData());
+        }
+    }
+
+    // check if protected block
+    private static boolean isProtectedBlock(int id) {
+        int[] ids = { 14, 15, 16, 21, 48, 54, 56, 73, 74, 98, 129 };
+
+        for (int i = 0; i < ids.length; i++) {
+            if (id == ids[i]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
---
+++ src/main/java/org/tulonsae/antixray/BlockBreakListener.java
@@ -0,0 +1,67 @@
+package org.tulonsae.antixray;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import net.escapecraft.escapePlug.EscapePlug;
+import org.tulonsae.mc.util.Log;
+
+/**
+ * Handle BlockPlaceEvent.
+ */
+public class BlockBreakListener implements Listener {
+
+    private Log log;
+    private EscapePlug plugin;
+
+    private boolean updatePlayerOnlyChanges = true;
+    private double nearby = 0;
+
+    public BlockBreakListener(AntiXrayComponent component) {
+        this.plugin = component.getPlugin();
+        this.log = component.getLogger();
+
+        nearby = (plugin.getServer().getViewDistance() + 1) * 16;
+        // use the square of the distance rather than the distance
+        // to avoid costly sqrt calculations
+        nearby = nearby * nearby;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        // check whether to process only player changes
+        // in other words, no WE changes
+        if (updatePlayerOnlyChanges && (event.getPlayer() == null)) {
+            return;
+        }
+
+        // check whether tracking this world
+        Block block = event.getBlock();
+        World world = block.getWorld();
+        if (!AntiXrayComponent.isMonitoredWorld(world)) {
+            return;
+        }
+
+        // update the nearby players
+        Player[] allPlayers = plugin.getServer().getOnlinePlayers();
+        for (Player player : allPlayers) {
+            if (player.getWorld() == world) {
+                // note: using square of the distance for comparison
+                double distance = player.getLocation().distanceSquared(block.getLocation());
+                if (distance < nearby) {
+                    AntiXrayComponent.updatePlayer(player, block);
+                }
+            }
+        }
+    }
+}
