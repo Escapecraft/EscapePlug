41de02d9e5bfede1ee14bfe42fe32719422b0a8f
James <beardyjames@live.co.uk>
2013-02-03 19:26:30 +0000

    Display time until kit is usable

--- src/main/java/com/tehbeard/kit/KitCommand.java
+++ src/main/java/com/tehbeard/kit/KitCommand.java
@@ -36,22 +36,30 @@ public class KitCommand implements CommandExecutor{
 			}
 			sender.sendMessage(msg);
 		}
-		
+
 		if(args.length==1 && sender instanceof Player){
 			Kit kit = dataManager.getKit(args[0]);
 			if(kit == null){
 				sender.sendMessage(ChatColor.RED + " Kit does not exist");
 				return true;
 			}
-			
+
 			switch(kit.giveKit((Player)sender)){
 			case OK:sender.sendMessage("Enjoy your kit!");dataManager.saveData();break;
 			case PERM:sender.sendMessage("You can't use that kit!");break;
-			case TIMER:sender.sendMessage("You must wait till you can use that kit!");break;
+			case TIMER:
+				for(KitContext kc : KitContext.getContexts(((Player)sender).getName())){
+					if(kc.getName().equalsIgnoreCase(args[0])){
+						float hoursTill = (kc.timeTill() / 3600);
+						sender.sendMessage("You must wait " + hoursTill + " hours till you can use that kit!");
+					}
+					break;
+				}
+				break;
 			}
 		}
 
-	return true;
-}
+		return true;
+	}
 
 }
